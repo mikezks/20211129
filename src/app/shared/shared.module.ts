5482bf16f02b8a0a59a98de6e061cb8c50a8c45c
michael.zikes <michael.zikes@intauria.com>
2021-11-30 20:10:55 +0100

    Pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CityPipe } from './pipes/city.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CityPipe
+  ],
   imports: [
     CommonModule,
     FormsModule
   ],
   exports: [
-    FormsModule
+    FormsModule,
+    CityPipe
   ]
 })
 export class SharedModule { }
